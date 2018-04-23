a569afcf61791cf4318f6116f85202fea7a22dda
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SpecifyPageRange/Form1.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Windows.Forms;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+// ...
+
+namespace SpecifyPageRange {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            XtraReport1 report = new XtraReport1();
+            report.PrintingSystem.StartPrint +=
+                new PrintDocumentEventHandler(printingSystem_StartPrint);
+            report.Print();
+        }
+
+        private void printingSystem_StartPrint(object sender, PrintDocumentEventArgs e) {
+            // Set the page range.
+            e.PrintDocument.PrinterSettings.FromPage = 1;
+            e.PrintDocument.PrinterSettings.ToPage = 3;
+        }
+
+    }
+}
